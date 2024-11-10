6bcdad1027e02f687dd378a98c2edfcc87e65a25
define42 <127189851+define42@users.noreply.github.com>
2024-11-10 22:21:59 +0100

    Update podman-entrypoint.sh

--- podman-entrypoint.sh
+++ podman-entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
 
+if [ -f "${CA_CERTIFICATES_PATH}" ]; then
+  echo "Custom CA certificate found at ${CA_CERTIFICATES_PATH}."
+  echo "Copying certificate to /etc/pki/ca-trust/source/anchors/..."
+  cp "${CA_CERTIFICATES_PATH}" /etc/pki/ca-trust/source/anchors/
+  
+  echo "Updating the CA trust store..."
+  update-ca-trust
+  echo "CA trust store updated successfully."
+else
+  echo "No custom CA certificate found at ${CA_CERTIFICATES_PATH}."
+fi
+
 # Execute CMD
 #/usr/bin/podman system service --time=0 unix:/home/podmanuser/podman/podman.sock
 exec "$@"
