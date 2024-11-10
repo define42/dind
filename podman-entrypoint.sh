84824e2079a0a5c6c8f578d97c27fc3e6d6a0f34
define42 <127189851+define42@users.noreply.github.com>
2024-11-10 16:13:43 +0100

    Update podman-entrypoint.sh

--- podman-entrypoint.sh
+++ podman-entrypoint.sh
@@ -2,4 +2,5 @@
 set -e
 
 # Execute CMD
-/usr/bin/podman system service --time=0 unix:/home/podmanuser/podman/podman.sock
+#/usr/bin/podman system service --time=0 unix:/home/podmanuser/podman/podman.sock
+exec "$@"
